a7b4a594bcf6e18ab847fec6919065bbddab2eb9
David Zhang <dzhang2@ece001.ece.local.cmu.edu>
2024-04-18 14:14:28 -0400

    working basic mult in task 4

--- DVEfiles/session_2024-04-18T13_37_51.tcl
+++ DVEfiles/session_2024-04-18T13_37_51.tcl
@@ -1,13 +1,13 @@
 # Begin_DVE_Session_Save_Info
 # DVE full session
-# Saved on Thu Apr 18 13:48:52 2024
+# Saved on Thu Apr 18 13:37:51 2024
 # Designs open: 1
 #   Sim: /afs/ece.cmu.edu/usr/dzhang2/Private/18240/lab5/simv
 # Toplevel windows open: 1
 # 	TopLevel.1
 #   Source.1: Decoder
 #   Group count = 1
-#   Group Group1 signal count = 37
+#   Group Group1 signal count = 32
 # End_DVE_Session_Save_Info
 
 # DVE version: T-2022.06_Full64
@@ -66,7 +66,7 @@ if {![gui_exist_window -window TopLevel.1]} {
 } else { 
     set TopLevel.1 TopLevel.1
 }
-gui_show_window -window ${TopLevel.1} -show_state normal -rect {{8 32} {1251 647}}
+gui_show_window -window ${TopLevel.1} -show_state normal -rect {{8 32} {1252 648}}
 
 # ToolBar settings
 gui_set_toolbar_attributes -toolbar {TimeOperations} -dock_state top
@@ -125,11 +125,11 @@ gui_set_window_pref_key -window ${DLPane.1} -key dock_width -value_type integer 
 gui_set_window_pref_key -window ${DLPane.1} -key dock_height -value_type integer -value 392
 gui_set_window_pref_key -window ${DLPane.1} -key dock_offset -value_type integer -value 0
 gui_update_layout -id ${DLPane.1} {{left 0} {top 0} {width 105} {height 392} {dock_state left} {dock_on_new_line true} {child_data_colvariable 140} {child_data_colvalue 100} {child_data_coltype 40} {child_data_col1 0} {child_data_col2 1} {child_data_col3 2}}
-set Console.1 [gui_create_window -type Console -parent ${TopLevel.1} -dock_state bottom -dock_on_new_line true -dock_extent 125]
+set Console.1 [gui_create_window -type Console -parent ${TopLevel.1} -dock_state bottom -dock_on_new_line true -dock_extent 126]
 gui_set_window_pref_key -window ${Console.1} -key dock_width -value_type integer -value 1220
-gui_set_window_pref_key -window ${Console.1} -key dock_height -value_type integer -value 125
+gui_set_window_pref_key -window ${Console.1} -key dock_height -value_type integer -value 126
 gui_set_window_pref_key -window ${Console.1} -key dock_offset -value_type integer -value 0
-gui_update_layout -id ${Console.1} {{left 0} {top 0} {width 1243} {height 124} {dock_state bottom} {dock_on_new_line true}}
+gui_update_layout -id ${Console.1} {{left 0} {top 0} {width 1244} {height 125} {dock_state bottom} {dock_on_new_line true}}
 #### Start - Readjusting docked view's offset / size
 set dockAreaList { top left right bottom }
 foreach dockArea $dockAreaList {
@@ -173,11 +173,11 @@ gui_update_statusbar_target_frame ${TopLevel.1}
 # DVE Open design session: 
 
 if { [llength [lindex [gui_get_db -design Sim] 0]] == 0 } {
-gui_set_env SIMSETUP::SIMARGS {{-ucligui }}
-gui_set_env SIMSETUP::SIMEXE {/afs/ece.cmu.edu/usr/dzhang2/Private/18240/lab5/simv}
+gui_set_env SIMSETUP::SIMARGS {{}}
+gui_set_env SIMSETUP::SIMEXE {./simv}
 gui_set_env SIMSETUP::ALLOW_POLL {0}
 if { ![gui_is_db_opened -db {/afs/ece.cmu.edu/usr/dzhang2/Private/18240/lab5/simv}] } {
-gui_sim_run Ucli -exe simv -args {-ucligui } -dir /afs/ece.cmu.edu/usr/dzhang2/Private/18240/lab5 -nosource
+gui_sim_run Ucli -exe simv -args { -ucligui} -dir /afs/ece.cmu.edu/usr/dzhang2/Private/18240/lab5 -nosource
 }
 }
 if { ![gui_sim_state -check active] } {error "Simulator did not start correctly" error}
@@ -202,11 +202,11 @@ gui_set_time_units 1s
 gui_load_child_values {test_bench.DUT}
 
 
-set _session_group_2 Group1
-gui_sg_create "$_session_group_2"
-set Group1 "$_session_group_2"
+set _session_group_1 Group1
+gui_sg_create "$_session_group_1"
+set Group1 "$_session_group_1"
 
-gui_sg_addsignal -group "$_session_group_2" { test_bench.DUT.start test_bench.DUT.reset test_bench.DUT.clock test_bench.DUT.a test_bench.DUT.b test_bench.DUT.out test_bench.DUT.done test_bench.DUT.ZN_flags test_bench.DUT.up test_bench.DUT.load test_bench.DUT.D test_bench.DUT.en_counter test_bench.DUT.clear_counter test_bench.DUT.count test_bench.DUT.load_a test_bench.DUT.load_b test_bench.DUT.sel test_bench.DUT.sel_a test_bench.DUT.AeqB test_bench.DUT.AgtB test_bench.DUT.b_abs test_bench.DUT.not_b test_bench.DUT.a_out test_bench.DUT.b_out test_bench.DUT.a_new test_bench.DUT.b_new test_bench.DUT.not_product test_bench.DUT.product test_bench.DUT.A_shifted test_bench.DUT.B_shifted test_bench.DUT.load_product test_bench.DUT.clear_product test_bench.DUT.product_in test_bench.DUT.select_add_amount test_bench.DUT.num_to_add test_bench.DUT.not_needed test_bench.DUT.a_register.Q }
+gui_sg_addsignal -group "$_session_group_1" { test_bench.DUT.start test_bench.DUT.reset test_bench.DUT.clock test_bench.DUT.a test_bench.DUT.b test_bench.DUT.out test_bench.DUT.done test_bench.DUT.ZN_flags test_bench.DUT.up test_bench.DUT.load test_bench.DUT.D test_bench.DUT.en_counter test_bench.DUT.clear_counter test_bench.DUT.count test_bench.DUT.sel test_bench.DUT.sel_a test_bench.DUT.AeqB test_bench.DUT.AgtB test_bench.DUT.b_abs test_bench.DUT.not_b test_bench.DUT.a_new test_bench.DUT.b_new test_bench.DUT.not_product test_bench.DUT.product test_bench.DUT.A_shifted test_bench.DUT.B_shifted test_bench.DUT.load_product test_bench.DUT.clear_product test_bench.DUT.product_in test_bench.DUT.select_add_amount test_bench.DUT.num_to_add test_bench.DUT.not_needed }
 
 # Global: Highlighting
 
@@ -216,7 +216,7 @@ gui_change_stack_mode -mode list
 # Post database loading setting...
 
 # Restore C1 time
-gui_set_time -C1_only 325
+gui_set_time -C1_only 625
 
 
 
@@ -242,21 +242,20 @@ gui_list_set_filter -id ${Hier.1} -text {*}
 gui_hier_list_init -id ${Hier.1}
 gui_change_design -id ${Hier.1} -design Sim
 catch {gui_list_expand -id ${Hier.1} test_bench}
-catch {gui_list_expand -id ${Hier.1} test_bench.DUT}
-catch {gui_list_select -id ${Hier.1} {test_bench.DUT.a_register}}
-gui_view_scroll -id ${Hier.1} -vertical -set 138
-gui_view_scroll -id ${Hier.1} -horizontal -set 40
+catch {gui_list_select -id ${Hier.1} {test_bench.DUT}}
+gui_view_scroll -id ${Hier.1} -vertical -set 0
+gui_view_scroll -id ${Hier.1} -horizontal -set 0
 
 # Data 'Data.1'
 gui_list_set_filter -id ${Data.1} -list { {Buffer 1} {Input 1} {Others 1} {Linkage 1} {Output 1} {LowPower 1} {Parameter 1} {All 1} {Aggregate 1} {LibBaseMember 1} {Event 1} {Assertion 1} {Constant 1} {Interface 1} {BaseMembers 1} {Signal 1} {$unit 1} {Inout 1} {Variable 1} }
 gui_list_set_filter -id ${Data.1} -text {*}
-gui_list_show_data -id ${Data.1} {test_bench.DUT.a_register}
+gui_list_show_data -id ${Data.1} {test_bench.DUT}
 gui_show_window -window ${Data.1}
-catch { gui_list_select -id ${Data.1} {test_bench.DUT.a_register.Q }}
-gui_view_scroll -id ${Data.1} -vertical -set 0
+catch { gui_list_select -id ${Data.1} {test_bench.DUT.start test_bench.DUT.reset test_bench.DUT.clock test_bench.DUT.a test_bench.DUT.b test_bench.DUT.out test_bench.DUT.done test_bench.DUT.ZN_flags test_bench.DUT.up test_bench.DUT.load test_bench.DUT.D test_bench.DUT.en_counter test_bench.DUT.clear_counter test_bench.DUT.count test_bench.DUT.sel test_bench.DUT.sel_a test_bench.DUT.AeqB test_bench.DUT.AgtB test_bench.DUT.b_abs test_bench.DUT.not_b test_bench.DUT.a_new test_bench.DUT.b_new test_bench.DUT.not_product test_bench.DUT.product test_bench.DUT.A_shifted test_bench.DUT.B_shifted test_bench.DUT.load_product test_bench.DUT.clear_product test_bench.DUT.product_in test_bench.DUT.select_add_amount test_bench.DUT.num_to_add test_bench.DUT.not_needed }}
+gui_view_scroll -id ${Data.1} -vertical -set 271
 gui_view_scroll -id ${Data.1} -horizontal -set 0
-gui_view_scroll -id ${Hier.1} -vertical -set 138
-gui_view_scroll -id ${Hier.1} -horizontal -set 40
+gui_view_scroll -id ${Hier.1} -vertical -set 0
+gui_view_scroll -id ${Hier.1} -horizontal -set 0
 
 # Source 'Source.1'
 gui_src_value_annotate -id ${Source.1} -switch false
